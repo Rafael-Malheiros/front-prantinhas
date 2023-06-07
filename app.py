02671f82b7d25fb5449de4c260c26799adda74dc
Luscas-Fernandes <lfaa2057@gmail.com>
2023-06-06 22:30:35 -0300

    Major update
    Get funcional, html dinamico, post sem hash, css atualizado, rota principal atualizada

--- app.py
+++ app.py
@@ -8,29 +8,46 @@ import json
 app = Flask(__name__)
 firebase = firebase.FirebaseApplication('https://sd-cesar-7789b-default-rtdb.firebaseio.com/', None)
 link = "https://sd-cesar-7789b-default-rtdb.firebaseio.com/"
-hoje_dia = time.strftime("%d-%m-%Y")
-agora = datetime.now()
-hora = agora.strftime("%H:%M:%S")
-infos_tempo = hoje_dia + ' ' + hora
 
 
 @app.route('/')
+def red():
+    return redirect('/welcome')
+
+
+@app.route('/welcome')
 def welcome():
     return render_template('index.html')
 
 
 @app.route('/plantinhas', methods=['GET', 'POST'])
 def plants():
+    # Dando get no nome das plantas
+    plants = []
+    requisition = requests.get(f'{link}/plantas/.json')
+    dic = requisition.json()
+    for i in dic:
+        plants.append(i)
+    # Tentando botar pop-up javaScript na tela, caso planta já esteja cadastrada
+    nome_ja_registrado = 0
+    # Método post pra adicionar nova planta
     if request.method == 'POST':
-        nome_nova_planta = request.form['nome_planta']
-        requisicao_planta_nova = requests.post(f'{link}/plantas/{nome_nova_planta}.json', data=json.dumps('hash ind.'))
+        hoje_dia = time.strftime("%d-%m-%Y") # Pegar hora adicionada
+        agora = datetime.now() # Pegar hora adicionada
+        hora = agora.strftime("%H:%M:%S") # Pegar hora adicionada
+        infos_tempo = hoje_dia + ' ' + hora # Pegar hora adicionada
 
-        print(requisicao_planta_nova)
-    else:
-        return render_template('plantas.html')
+        nome_nova_planta = request.form['nome_planta'].capitalize() # Pegando nome do html
 
-    return render_template('plantas.html')
+        if nome_nova_planta not in dic: # Conferindo se planta não já está no banco pelo nome dela e se não, inserindo no banco
+            infos_tempo_inserir = {'data': infos_tempo}
+            requests.patch(f'{link}/plantas/{nome_nova_planta}/.json', data=json.dumps(infos_tempo_inserir))
+            return redirect('/plantinhas')
+        else: # Else, nao precisa de explicaçao
+            print("nome já registrado")
+            nome_ja_registrado = 1
+    return render_template('plantas.html', nome_ja_registrado=nome_ja_registrado, dic=dic)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
